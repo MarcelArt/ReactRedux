51ebb7bd296ceb337f039c92238cd3c428bd19cc
MarcelArt <marcel.aritonang@gmail.com>
2020-01-23 10:39:24 +0700

    commit 2
    jadi counter app pake redux

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-/**
- * @format
- */
+if (__DEV__) {
+  import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
+}
 
+import React, {Component} from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+import App from './src/App.js';
+export default class ReduxCounter extends Component {
+  render() {
+    return <App />;
+  }
+}
+AppRegistry.registerComponent('ReactRedux', () => ReduxCounter);
